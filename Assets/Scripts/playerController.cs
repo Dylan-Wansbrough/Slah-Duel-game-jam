27b30b35f3a9209bd03d31a4d8022e09a981b3af
Dylan <dylan.wansbrough@hotmail.com>
2021-10-13 13:12:29 +1300

    Initial player controller script
    giving the script for player

---
+++ Assets/Scripts/playerController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerController : MonoBehaviour
+{
+    //player 
+    public int playerNumber;
+
+    //Players state
+    public bool isAttacking;
+    public bool isBlocking;
+    public bool isFaking;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
