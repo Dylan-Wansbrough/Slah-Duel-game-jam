374ef00e12c59477f1ee70ce3851517fea53ffff
Ben <benjaminjsaddler@gmail.com>
2021-10-14 14:25:45 +1300

    Main Menu Fonts, Game Over screen

--- Assets/Scripts/MenuManager.cs
+++ Assets/Scripts/MenuManager.cs
@@ -5,7 +5,6 @@ using UnityEngine.SceneManagement;
 
 public class MenuManager : MonoBehaviour
 {
-    // Start is called before the first frame update
     public void PlayGame()
     {
         SceneManager.LoadScene(1);
